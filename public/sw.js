a7c2f68acb4f509acc29a623717427890d977a7e
seronayon <shinya4869ari@gmail.com>
2026-09-26 17:00:33 +0900

    feat: 歌詞モードのアーティスト別2階層管理（Spotify風）＆ ヘッダーのアーティスト名・YouTubeボタン追加 (v1.2.6)

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-// K-Learner - Service Worker (PWA) v12
-const CACHE_NAME = "klearner-pwa-v12";
+// K-Learner - Service Worker (PWA) v13
+const CACHE_NAME = "klearner-pwa-v13";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
